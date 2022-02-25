0b7f0d604159c4be051c06605edc52b2e1cb69f2
Adrian Furmanek <adrian.furm@gmail.com>
2022-02-26 00:48:11 +0100

    display photos

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log('test');
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+const count = 10;
+const apiKey ='ZPZ53mDTKZDK-M6XYvCDEbrmtM93BGbdVfojW7n8eCg';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.description,
+            title: photo.description
+        });
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+    });
+}
+
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        console.log(photosArray);
+        displayPhotos();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// OnLoad
+getPhotos();
